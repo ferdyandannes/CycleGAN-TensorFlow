2be47f7b835b69a3e6a8ec8aa6fcacbb336bd248
ferdyandannes <fdannes@gmail.com>
2019-06-17 11:13:11 +0800

    Update discriminator.py

--- discriminator.py
+++ discriminator.py
@@ -21,16 +21,49 @@ class Discriminator:
       # convolution layers
       C64 = ops.Ck(input, 64, reuse=self.reuse, norm=None,
           is_training=self.is_training, name='C64')             # (?, w/2, h/2, 64)
-      C128 = ops.Ck(C64, 128, reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C128')            # (?, w/4, h/4, 128)
-      C256 = ops.Ck(C128, 256, reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C256')            # (?, w/8, h/8, 256)
-      C512 = ops.Ck(C256, 512,reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C512')            # (?, w/16, h/16, 512)
+
+      #### Residual Network ####
+      #filter_shape = [1, 128, 128, 64]
+      # = create_variables(name='conv', shape=filter_shape)
+      #conv = tf.nn.conv2d(C64, filter,
+      #  strides=[1, 1, 1, 1], padding='SAME')
+
+      C64_res_1 = ops.Rk(C64, 64, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C64_res_1')
+
+      C64_res_2 = ops.Rk(C64_res_1, 64, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C64_res_2')
+
+      C128_norm = ops.Ck(C64_res_2, 128, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C128_norm')
+
+      C128_res_1 = ops.Rk(C128_norm, 128, reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C128_res_1')            # (?, w/4, h/4, 128)
+
+      C128_res_2 = ops.Rk(C128_res_1, 128, reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C128_res_2')
+
+      C256_norm = ops.Ck(C128_res_2, 256, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C256_norm')
+
+      C256_res_1 = ops.Rk(C256_norm, 256, reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C256_res_1')            # (?, w/8, h/8, 256)
+
+      C256_res_2 = ops.Rk(C256_res_1, 256, reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C256_res_2')            # (?, w/8, h/8, 256)
+
+      C512_norm = ops.Ck(C256_res_2, 512, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C512_norm')
+
+      C512_res_1 = ops.Rk(C512_norm, 512,reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C512_res_1')            # (?, w/16, h/16, 512)
+
+      C512_res_2 = ops.Rk(C512_res_1, 512,reuse=self.reuse, norm=self.norm,
+          is_training=self.is_training, name='C512_res_2')            # (?, w/16, h/16, 512)
 
       # apply a convolution to produce a 1 dimensional output (1 channel?)
       # use_sigmoid = False if use_lsgan = True
-      output = ops.last_conv(C512, reuse=self.reuse,
+      output = ops.last_conv(C512_res_2, reuse=self.reuse,
           use_sigmoid=self.use_sigmoid, name='output')          # (?, w/16, h/16, 1)
 
     self.reuse = True
